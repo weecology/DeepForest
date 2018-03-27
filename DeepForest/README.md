e61b824859ef77c061a430e52d19d3acddd657f3
bw4sz <benweinstein2010@gmail.com>
2018-03-27 10:47:41 -0700

    reorganized, made travis file

---
+++ DeepForest/README.md
@@ -0,0 +1,20 @@
+# DeepForest
+
+[![Build status](https://travis-ci.org/weecology/DeepForest.svg?master)](https://travis-ci.org/weecology)
+
+Segmentation of tree crowns using airborne lidar, orthophotos and hyperspectral data for NEON sites
+
+## Usage
+
+## Installation
+
+
+### Requirements
+
+### Compatibility
+
+### Licence
+
+### Authors
+
+`DeepForest` was written by `Ben Weinstein <ben.weinstein@weecology.org>`_.
--- HPC/StartDask.py
+++ HPC/StartDask.py
@@ -1,7 +1,34 @@
-from dask_jobqueue import SLURMCluster
+#srun --ntasks=1 --cpus-per-task=2 --mem=2gb -t 90 --pty bash -i
 
-cluster = SLURMCluster(project='ewhite')
-cluster.start_workers(3)
+from dask_jobqueue import SLURMCluster
+from datetime import datetime
+from time import sleep
+
+cluster = SLURMCluster(project='ewhite',death_timeout=100)
+cluster.start_workers(1)
+
+print(cluster.job_script())
 
 from dask.distributed import Client
 client = Client(cluster)
+
+client
+
+counter=0
+while counter < 10:
+    print(datetime.now().strftime("%a, %d %B %Y %I:%M:%S"))
+    print(client)
+    sleep(20)
+    counter+=1
+
+import socket
+host = client.run_on_scheduler(socket.gethostname)
+
+def start_jlab(dask_scheduler):
+    import subprocess
+    proc = subprocess.Popen(['jupyter', 'lab', '--ip', host, '--no-browser'])
+    dask_scheduler.jlab_proc = proc
+
+client.run_on_scheduler(start_jlab)
+
+print("ssh -N -L 8787:%s:8787 -L 8888:%s:8888 -l b.weinstein hpg2.rc.ufl.edu" % (host, host))
