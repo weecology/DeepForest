dd41a6a0863b73c15f06c90e9431aece83cf79e4
bw4sz <benweinstein2010@gmail.com>
2018-03-30 13:29:18 -0700

    working on dependencies for travis env

---
+++ tests/install-dependencies.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env sh
+
+module load git
+git clone https://github.com/LASzip/LASzip.git
+cd LASzip
+git checkout tags/2.0.2
+
+mkdir build
+cd build
+module load cmake
+module load gcc
+cmake .. 
+make
+make install
+
+cd ~
+wget http://lastools.org/download/LAStools.zip
+unzip LAStools.zip
+cd LAStools
+make
+
+cd /usr/local/LAStools/bin
+cp laszip /user/b.weinstein/LASzip/build/bin/
+cd /usr/local/LASzip/build/bin/
+ln -s /usr/local/LASzip/build/bin/laszip /usr/local/LASzip/build/bin/laszip-cli
--- tests/test_lidar.py
+++ tests/test_lidar.py
@@ -1,3 +1,3 @@
 def test_lidar():
-    test_class=lidar()
-    assert(test_class.filename=)
+    test_class=lidar(filename="data/NEON_D03_OSBS_DP1_405000_3276000_classified_point_cloud.laz")
+    assert(test_class.filename=="data/NEON_D03_OSBS_DP1_405000_3276000_classified_point_cloud.laz")
