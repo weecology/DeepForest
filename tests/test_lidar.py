5858d4775e619b340d746cd967e2d74fa3f8c212
bw4sz <benweinstein2010@gmail.com>
2018-03-30 19:14:39 -0700

    classes have cap letters

--- tests/test_lidar.py
+++ tests/test_lidar.py
@@ -1,3 +1,3 @@
 def test_Lidar():
-    test_class=lidar(filename="data/NEON_D03_OSBS_DP1_405000_3276000_classified_point_cloud.laz")
+    test_class=Lidar(filename="data/NEON_D03_OSBS_DP1_405000_3276000_classified_point_cloud.laz")
     assert(test_class.filename=="data/NEON_D03_OSBS_DP1_405000_3276000_classified_point_cloud.laz")
