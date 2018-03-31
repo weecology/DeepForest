ad5ece7218107047ef8c4854db7bdc454b3aa170
bw4sz <benweinstein2010@gmail.com>
2018-03-31 13:13:47 -0700

    assert file is read in

--- DeepForest/Lidar.py
+++ DeepForest/Lidar.py
@@ -1,4 +1,6 @@
+import laspy
+
 class Lidar:
     def __init__(self,filename):
         self.filename=filename
-        
+        self.tile = laspy.file.File(filename)        
--- tests/test_lidar.py
+++ tests/test_lidar.py
@@ -1,5 +1,5 @@
 from DeepForest.Lidar import Lidar
 
 def test_Lidar():
-    test_class=Lidar(filename="data/NEON_D03_OSBS_DP1_405000_3276000_classified_point_cloud.laz")
-    assert(test_class.filename=="data/NEON_D03_OSBS_DP1_405000_3276000_classified_point_cloud.laz")
+    test_class=Lidar(filename="tests/data/NEON_D03_OSBS_DP1_405000_3276000_classified_point_cloud.laz")
+    assert(len(test_class.tile.points)==2912035)
