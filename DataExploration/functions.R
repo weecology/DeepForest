0307d4cdec11a0864fd34ce0dcec044d8479051f
bw4sz <benweinstein2010@gmail.com>
2018-04-06 15:16:01 -0700

    extract individual tree

--- DataExploration/functions.R
+++ DataExploration/functions.R
@@ -9,23 +9,58 @@ segment_ITC<-function(las,algorithm="watershed"){
   lasnormalize(las, method = "knnidw", k = 10L)
   
   # compute a canopy image
-  chm= grid_canopy (las, 1, subcircle = 0.1, na.fill = "knnidw", k = 3, p = 2)
+  chm= grid_canopy (las, res=1, subcircle = 0.1, na.fill = "knnidw", k = 3, p = 2)
   chm = as.raster(chm)
   kernel = matrix(1,3,3)
   chm = raster::focal(chm, w = kernel, fun = mean)
   chm = raster::focal(chm, w = kernel, fun = mean)
   
-  # tree segmentation
-  crowns = lastrees(las, algorithm = algorithm, chm, th = 4, extra = TRUE)
+  if (algorithm=="dalponte2016"){
+    
+    # Dalponte 2016
+    ttops = tree_detection(chm, 5, 2)
+    crowns <- lastrees_dalponte(las, chm, ttops,extra=T)
+    plot(crowns, color = "treeID", colorPalette = col)
+    
+    contour = rasterToPolygons(crowns, dissolve = TRUE)
+    
+    plot(chm, col = height.colors(50))
+    plot(contour, add = T)
+    return(list(polygons=contour,raster=crowns,tops=ttops))
+  }
+
+  if(algorithm=="watershed"){
+    # tree segmentation
+    crowns = lastrees(las, algorithm = algorithm, chm, th = 4, extra = TRUE)
+    
+    # display
+    tree = lasfilter(las, !is.na(treeID))
+    plot(tree, color = "treeID", colorPalette = pastel.colors(100), size = 1)
+    
+    # More stuff
+    contour = rasterToPolygons(crowns, dissolve = TRUE)
+    
+    plot(chm, col = height.colors(50))
+    plot(contour, add = T)
+    return(list(polygons=contour,raster=crowns))
+  }
   
-  # display
-  tree = lasfilter(las, !is.na(treeID))
-  plot(tree, color = "treeID", colorPalette = pastel.colors(100), size = 1)
-  
-  # More stuff
-  contour = rasterToPolygons(crowns, dissolve = TRUE)
-  
-  plot(chm, col = height.colors(50))
-  plot(contour, add = T)
-  return(contour)
+  if(algorithm=="silva2016"){
+    
+    ttops = tree_detection(chm, 5, 2)
+    crowns<-lastrees_silva(las, chm, ttops, max_cr_factor = 0.6, exclusion = 0.3,
+                   extra = T)
+    
+    # display
+    tree = lasfilter(las, !is.na(treeID))
+    plot(tree, color = "treeID", colorPalette = pastel.colors(100), size = 1)
+    
+    # More stuff
+    contour = rasterToPolygons(crowns, dissolve = TRUE)
+    
+    plot(chm, col = height.colors(50))
+    plot(contour, add = T)
+    return(list(polygons=contour,raster=crowns,tops=ttops))
+  }
+
 }
