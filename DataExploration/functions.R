d0b0564697a3e8a1df28f160be70496577b594bd
bw4sz <benweinstein2010@gmail.com>
2018-04-11 13:35:14 -0700

    4th method and some sanity filters

--- DataExploration/functions.R
+++ DataExploration/functions.R
@@ -1,20 +1,40 @@
-segment_ITC<-function(las,algorithm="watershed"){
-  
+ground_model<-function(las){
   ws = seq(3,21, 3)
-  th = seq(0.1, 2, length.out = length(ws))
+  th = seq(0.1, 6, length.out = length(ws))
   
   lasground(las, "pmf", ws, th)
   
   # normalization
-  lasnormalize(las, method = "knnidw", k = 10L)
-  
+  lasnormalize(las, method = "kriging", k = 10L, model = gstat::vgm(0.59, "Sph", 874))
+}
+
+canopy_model<-function(las){
   # compute a canopy image
   chm= grid_canopy (las, res=1, subcircle = 0.1, na.fill = "knnidw", k = 3, p = 2)
   chm = as.raster(chm)
   kernel = matrix(1,3,3)
   chm = raster::focal(chm, w = kernel, fun = mean)
   chm = raster::focal(chm, w = kernel, fun = mean)
+  return(chm)
+}
+
+segment_ITC<-function(las,algorithm="watershed",chm=chm){
   
+  if(algorithm=="watershed"){
+    # tree segmentation
+    crowns = lastrees(las, algorithm = algorithm, chm, th = 4, extra = TRUE)
+    
+    # display
+    tree = lasfilter(las, !is.na(treeID))
+    plot(tree, color = "treeID", colorPalette = pastel.colors(100), size = 1)
+    
+    # More stuff
+    contour = rasterToPolygons(crowns, dissolve = TRUE)
+    
+    plot(chm, col = height.colors(50))
+    plot(contour, add = T)
+    return(las)
+  }
   if (algorithm=="dalponte2016"){
     
     # Dalponte 2016
@@ -29,19 +49,14 @@ segment_ITC<-function(las,algorithm="watershed"){
     return(las)
   }
 
-  if(algorithm=="watershed"){
+  if(algorithm=="li2012"){
     # tree segmentation
-    crowns = lastrees(las, algorithm = algorithm, chm, th = 4, extra = TRUE)
+    lastrees(las, "li2012", R = 5)
     
     # display
     tree = lasfilter(las, !is.na(treeID))
     plot(tree, color = "treeID", colorPalette = pastel.colors(100), size = 1)
     
-    # More stuff
-    contour = rasterToPolygons(crowns, dissolve = TRUE)
-    
-    plot(chm, col = height.colors(50))
-    plot(contour, add = T)
     return(las)
   }
   
