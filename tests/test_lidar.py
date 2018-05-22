763f6636ea90320a151be99c5005e2d09d9385bf
bw4sz <benweinstein2010@gmail.com>
2018-05-22 17:40:18 -0400

    merging

--- tests/test_lidar.py
+++
@@ -1,5 +0,0 @@
-from DeepForest.Lidar import Lidar
-
-def test_Lidar():
-    test_class=Lidar(filename="tests/data/NEON_D03_OSBS_DP1_405000_3276000_classified_point_cloud.laz")
-    assert(len(test_class.tile.points)==2912035)
