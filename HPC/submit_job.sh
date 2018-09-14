5111969553400fe002874456d03e778f8bb27d45
bw4sz <benweinstein2010@gmail.com>
2018-09-14 13:53:19 -0700

    new container

--- HPC/submit_job.sh
+++ HPC/submit_job.sh
@@ -16,8 +16,6 @@
 #maybe export  HDF5_USE_FILE_LOCKING=FALSE for saving paths.
 
 ml git
-ml gcc
-ml gdal
 ml geos/3.6.2
 ml tensorflow/1.10.1
 export PYTHONPATH=${PYTHONPATH}:/home/b.weinstein/miniconda3/envs/DeepForest/lib/python3.6/site-packages/
