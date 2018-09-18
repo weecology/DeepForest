d6876cd4beb1297ccaec5f94a4c157580d13d66e
bw4sz <benweinstein2010@gmail.com>
2018-09-18 07:41:30 -0700

    not logging trees

--- eval.py
+++ eval.py
@@ -130,45 +130,45 @@ def main(config,experiment,args=None):
 
     print(model.summary())
 
-    average_precisions = evaluate(
-        generator,
-        model,
-        iou_threshold=args.iou_threshold,
-        score_threshold=args.score_threshold,
-        max_detections=args.max_detections,
-        save_path=args.save_path + dirname
-    )
+    #average_precisions = evaluate(
+        #generator,
+        #model,
+        #iou_threshold=args.iou_threshold,
+        #score_threshold=args.score_threshold,
+        #max_detections=args.max_detections,
+        #save_path=args.save_path + dirname
+    #)
 
-    # print evaluation
-    present_classes = 0
-    precision = 0
-    for label, (average_precision, num_annotations) in average_precisions.items():
-        print('{:.0f} instances of class'.format(num_annotations),
-              generator.label_to_name(label), 'with average precision: {:.4f}'.format(average_precision))
-        if num_annotations > 0:
-            present_classes += 1
-            precision       += average_precision
-    print('mAP: {:.4f}'.format(precision / present_classes))
-    experiment.log_metric("mAP", precision / present_classes)    
+    ## print evaluation
+    #present_classes = 0
+    #precision = 0
+    #for label, (average_precision, num_annotations) in average_precisions.items():
+        #print('{:.0f} instances of class'.format(num_annotations),
+              #generator.label_to_name(label), 'with average precision: {:.4f}'.format(average_precision))
+        #if num_annotations > 0:
+            #present_classes += 1
+            #precision       += average_precision
+    #print('mAP: {:.4f}'.format(precision / present_classes))
+    #experiment.log_metric("mAP", precision / present_classes)    
 
-    #Use field collected polygons only for Florida site
-    if site == "OSBS":
+    ##Use field collected polygons only for Florida site
+    #if site == "OSBS":
 
-        #Ground truth scores
-        jaccard_scores = JaccardEvaluate(
-            generator,
-            model,
-            iou_threshold=args.iou_threshold,
-            score_threshold=args.score_threshold,
-            max_detections=args.max_detections,
-            save_path=args.save_path + dirname,
-            experiment=experiment,
-            config=config
-        )
+        ##Ground truth scores
+        #jaccard_scores = JaccardEvaluate(
+            #generator,
+            #model,
+            #iou_threshold=args.iou_threshold,
+            #score_threshold=args.score_threshold,
+            #max_detections=args.max_detections,
+            #save_path=args.save_path + dirname,
+            #experiment=experiment,
+            #config=config
+        #)
         
-        print("Mean IoU = %.3f" % (jaccard_scores))
-        #Log results
-        experiment.log_metric("Mean IoU", jaccard_scores)    
+        #print("Mean IoU = %.3f" % (jaccard_scores))
+        ##Log results
+        #experiment.log_metric("Mean IoU", jaccard_scores)    
         
     ##Neon plot recall rate
     recall = neonRecall(
@@ -182,6 +182,8 @@ def main(config,experiment,args=None):
         experiment=experiment,
         config=config
     )    
+    
+    print(f" Recall: {recall:.2f}")
     
     experiment.log_metric("Recall", recall)    
     
