0642bb1c89842cf796d890a48437f80a6b25c9e4
bw4sz <benweinstein2010@gmail.com>
2018-09-26 13:07:41 -0700

    callbacks

---
+++ DeepForest/callbacks.py
@@ -0,0 +1,95 @@
+import keras
+
+from .evaluation import neonRecall, Jaccard
+
+class jaccardCallback(keras.callbacks.Callback):
+    """ Evaluation callback for arbitrary datasets.
+    """
+
+    def __init__(self, generator, iou_threshold=0.5, score_threshold=0.05, max_detections=100, suppression_threshold=0.2,save_path=None, weighted_average=False, verbose=1,experiment=None,DeepForest_config=None):
+        """ Evaluate a given dataset using a given model at the end of every epoch during training.
+
+        # Arguments
+            generator       : The generator that represents the dataset to evaluate.
+            iou_threshold   : The threshold used to consider when a detection is positive or negative.
+            score_threshold : The score confidence threshold to use for detections.
+            max_detections  : The maximum number of detections to use per image.
+            suppression_threshold:  Percent overlap allowed among boxes
+            save_path       : The path to save images with visualized detections to.
+            verbose         : Set the verbosity level, by default this is set to 1.
+            Experiment   : Comet ml experiment for online logging
+        """
+        self.generator       = generator
+        self.iou_threshold   = iou_threshold
+        self.score_threshold = score_threshold
+        self.max_detections  = max_detections
+        self.suppression_threshold=suppression_threshold
+        self.save_path       = save_path
+        self.weighted_average = weighted_average
+        self.verbose         = verbose
+        self.experiment = experiment
+        self.DeepForest_config = DeepForest_config
+
+        super(jaccardCallback, self).__init__()
+        
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+
+        jaccard=Jaccard(
+            generator=self.generator,
+            model=self.model,            
+            score_threshold=self.score_threshold,
+            save_path=self.save_path,
+            experiment=self.experiment,
+            DeepForest_config=self.DeepForest_config
+        )
+        
+        self.experiment.log_metric("Jaccard", jaccard)               
+        
+# Neon Recall 
+class recallCallback(keras.callbacks.Callback):
+    """ Evaluation callback for arbitrary datasets.
+    """
+
+    def __init__(self, site,generator, iou_threshold=0.5, score_threshold=0.05, max_detections=100, suppression_threshold=0.2,save_path=None, weighted_average=False, verbose=1,experiment=None,DeepForest_config=None):
+        """ Evaluate a given dataset using a given model at the end of every epoch during training.
+
+        # Arguments
+            generator       : The generator that represents the dataset to evaluate.
+            iou_threshold   : The threshold used to consider when a detection is positive or negative.
+            score_threshold : The score confidence threshold to use for detections.
+            max_detections  : The maximum number of detections to use per image.
+            suppression_threshold:  Percent overlap allowed among boxes
+            save_path       : The path to save images with visualized detections to.
+            verbose         : Set the verbosity level, by default this is set to 1.
+            Experiment   : Comet ml experiment for online logging
+        """
+        self.site                = site
+        self.generator       = generator
+        self.iou_threshold   = iou_threshold
+        self.score_threshold = score_threshold
+        self.max_detections  = max_detections
+        self.suppression_threshold=suppression_threshold
+        self.save_path       = save_path
+        self.weighted_average = weighted_average
+        self.verbose         = verbose
+        self.experiment = experiment
+        self.DeepForest_config = DeepForest_config
+
+        super(recallCallback, self).__init__()
+        
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+
+        recall=neonRecall(
+            self.site,
+            self.generator,
+            self.model,            
+            score_threshold=self.score_threshold,
+            save_path=self.save_path,
+            experiment=self.experiment,
+            DeepForest_config=self.DeepForest_config
+        )
+        
+        self.experiment.log_metric("Recall", recall)       
+
