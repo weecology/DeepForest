9e807bdf398be6287fc476de87ac6dbba2e1a605
bw4sz <benweinstein2010@gmail.com>
2018-11-28 13:05:24 -0500

    npt seeing what's differnet

--- HPC/submit_training.sh
+++ HPC/submit_training.sh
@@ -20,7 +20,7 @@ ml geos/3.6.2
 ml tensorflow/1.10.1
 export PYTHONPATH=${PYTHONPATH}:/home/b.weinstein/miniconda/envs/DeepLidar/lib/python3.6/site-packages/
 echo $PYTHONPATH
-sleep 30
+sleep 10
 
 cd /home/b.weinstein/DeepLidar
 which python
