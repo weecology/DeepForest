8e7946a676a0eeb7f3040cc888a486dcfad2a376
bw4sz <benweinstein2010@gmail.com>
2018-11-29 09:50:21 -0800

    trying chm again

--- profiler.py
+++ profiler.py
@@ -1,41 +1,49 @@
 #Profile Generator
 
-from DeepForest import onthefly_generator,preprocess, config
-
 import cProfile, pstats
 cp = cProfile.Profile()
 
-#Load data
-DeepForest_config=config.load_config("train")
-data=preprocess.load_data(DeepForest_config["training_csvs"],DeepForest_config["rgb_res"],DeepForest_config["lidar_path"])
-
-#Split training and test data
-train,test=preprocess.split_training(data,
-                                     DeepForest_config,
-                                     single_tile=DeepForest_config["single_tile"],
-                                     experiment=None)
-
-generator = onthefly_generator.OnTheFlyGenerator(
-    data,
-    train,
-    batch_size=5,
-    DeepForest_config=DeepForest_config,
-    group_method="none",
-shuffle_tile_epoch=True,
-name="training")
+from DeepForest import Lidar
 
 cp.enable()
-
-for i in range(generator.size()):
-    raw_image    = generator.load_image(i)
-
+lidar_tile=Lidar.load_lidar("/Users/ben/Documents/DeepLidar/data/SJER/NEON_D17_SJER_DP1_257000_4110000_classified_point_cloud_colorized.laz")
+lidar_tile.chm(0.1)
 cp.disable()
+
+#from DeepForest import onthefly_generator,preprocess, config
+
+
+##Load data
+#DeepForest_config=config.load_config("train")
+#data=preprocess.load_data(DeepForest_config["training_csvs"],DeepForest_config["rgb_res"],DeepForest_config["lidar_path"])
+
+##Split training and test data
+#train,test=preprocess.split_training(data,
+                                     #DeepForest_config,
+                                     #single_tile=DeepForest_config["single_tile"],
+                                     #experiment=None)
+
+#generator = onthefly_generator.OnTheFlyGenerator(
+    #data,
+    #train,
+    #batch_size=5,
+    #DeepForest_config=DeepForest_config,
+    #group_method="none",
+#shuffle_tile_epoch=True,
+#name="training")
+
+#cp.enable()
+
+#for i in range(generator.size()):
+    #raw_image    = generator.load_image(i)
+
+#cp.disable()
 
 stats = pstats.Stats(cp)
 stats.strip_dirs()
 stats.sort_stats('cumulative', 'calls')
 stats.print_stats(40)
 
-#Dump
-filename = 'profile.prof' 
-stats.dump_stats(filename)
+##Dump
+#filename = 'profile.prof' 
+#stats.dump_stats(filename)
