eb1e11cb295aa9d5289a6bb1bfe2b2d24d677eeb
bw4sz <benweinstein2010@gmail.com>
2018-11-30 11:39:57 -0800

    2 workers

--- profiler.py
+++ profiler.py
@@ -353,7 +353,7 @@ def main(args=None,data=None,DeepForest_config=None,experiment=None):
         verbose=1,
         shuffle=False,
     callbacks=None,
-    workers=1
+    workers=2
     )
     
     cp.disable()
