e678d23da2b5dc38811cc30aff3ad482804993dd
bw4sz <benweinstein2010@gmail.com>
2018-12-03 10:01:03 -0800

    wrapper script

--- HPC/submit_training.sh
+++ HPC/submit_training.sh
@@ -1,31 +1,4 @@
 #!/bin/bash
-stamp=$(date +%Y%m%d_%H%M%S) # time stamp
+stamp=$(date +%Y%m%d_%H%M%S) # time stamp for a directory to catch all logs.
 
-#SBATCH --job-name=DeepForest   # Job name
-#SBATCH --mail-type=END               # Mail events (NONE, BEGIN, END, FAIL, AL$
-#SBATCH --mail-user=benweinstein2010@gmail.com  # Where to send mail
-#SBATCH --account=ewhite
-#SBATCH --nodes=1                 # Number of MPI ranks
-#SBATCH --ntasks=1                 # Number of MPI ranks
-#SBATCH --cpus-per-task=5            # Number of cores per MPI rank
-#SBATCH --mem-per-cpu=10GB
-#SBATCH --time=48:00:00       #Time limit hrs:min:sec
-#SBATCH --output=/home/b.weinstein/logs/DeepForest_$stamp.out   # Standard output and error log
-#SBATCH --error=/home/b.weinstein/logs/DeepForest_$stamp.err
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:tesla:1
-
-#maybe export  HDF5_USE_FILE_LOCKING=FALSE for saving paths.
-
-ml git
-ml geos/3.6.2
-ml tensorflow/1.10.1
-export PATH=${PATH}:/home/b.weinstein/miniconda/envs/DeepLidar/bin/
-export PYTHONPATH=${PYTHONPATH}:/home/b.weinstein/miniconda/envs/DeepLidar/lib/python3.6/site-packages/
-echo $PYTHONPATH
-sleep 2
-
-cd /home/b.weinstein/DeepLidar
-which python
-
-python train.py --mode train --dir $stamp
+sbatch --job-name=$stamp.run --output=$stamp.out --export=stamp=$stamp /home/b.weinstein/DeepLidar/HPC/training.sbatch
