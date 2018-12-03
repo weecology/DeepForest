940b559fb67126fa64e21952a004bf8132f427a0
bw4sz <benweinstein2010@gmail.com>
2018-12-03 09:28:38 -0800

    direct error files

--- HPC/submit_training.sh
+++ HPC/submit_training.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+stamp=date "+%Y_%m_%d_%H_%M_%S" # time stamp
+
 #SBATCH --job-name=DeepForest   # Job name
 #SBATCH --mail-type=END               # Mail events (NONE, BEGIN, END, FAIL, AL$
 #SBATCH --mail-user=benweinstein2010@gmail.com  # Where to send mail
@@ -7,9 +9,9 @@
 #SBATCH --ntasks=1                 # Number of MPI ranks
 #SBATCH --cpus-per-task=5            # Number of cores per MPI rank
 #SBATCH --mem-per-cpu=10GB
-#SBATCH --time=24:00:00       #Time limit hrs:min:sec
-#SBATCH --output=/home/b.weinstein/logs/DeepForest.out   # Standard output and error log
-#SBATCH --error=/home/b.weinstein/logs/DeepForest.err
+#SBATCH --time=48:00:00       #Time limit hrs:min:sec
+#SBATCH --output=/home/b.weinstein/logs/DeepForest_$stamp.out   # Standard output and error log
+#SBATCH --error=/home/b.weinstein/logs/DeepForest_$stamp.err
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:tesla:1
 
@@ -19,7 +21,7 @@ ml git
 ml geos/3.6.2
 ml tensorflow/1.10.1
 export PATH=${PATH}:/home/b.weinstein/miniconda/envs/DeepLidar/bin/
-export PYTHONPATH=${PYTHONPATH}:/home/b.weinstein/miniconda/envs/DeepLidar/lib/python3.6/site-packages/:
+export PYTHONPATH=${PYTHONPATH}:/home/b.weinstein/miniconda/envs/DeepLidar/lib/python3.6/site-packages/
 echo $PYTHONPATH
 sleep 2
 
