71ad92789028f8bfd3ed6e6574a05a986e905c73
bw4sz <benweinstein2010@gmail.com>
2018-12-03 10:07:28 -0800

    chaange dir

--- HPC/submit_training.sh
+++ HPC/submit_training.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 stamp=$(date +%Y%m%d_%H%M%S) # time stamp for a directory to catch all logs.
 
-sbatch --job-name=$stamp.run --output=$stamp.out --export=stamp=$stamp /home/b.weinstein/DeepLidar/HPC/training.sbatch
+sbatch --job-name=$stamp.run --output=/home/b.weinstein/$stamp.out --export=stamp=/home/b.weinstein/$stamp /home/b.weinstein/DeepLidar/HPC/training.sbatch
