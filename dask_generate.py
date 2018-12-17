62dee55a130af43746958fb33783226f82f0292e
bw4sz <benweinstein2010@gmail.com>
2018-12-17 13:45:25 -0800

    remove host arg, scoping is confusing

--- dask_generate.py
+++ dask_generate.py
@@ -34,7 +34,7 @@ def run_local(data_paths):
 ### HiperGator ####
 
 #Start juypter notebook to watch
-def start_tunnel(host):
+def start_tunnel():
     """
     Start a juypter session and ssh tunnel to view task progress
     """
@@ -68,7 +68,7 @@ def run_HPC(data_paths):
     dask_client = Client(cluster)
         
     #Start dask dashboard? Not clear yet.
-    dask_client.run_on_scheduler(start_tunnel,host)    
+    dask_client.run_on_scheduler(start_tunnel)    
     
     # Local threading/processes, set scheduler.
     values = [delayed(Generate.run)(x) for x in data_paths]
