34c1e11b0b11cb75b46d38734c4daa865f109caa
bw4sz <benweinstein2010@gmail.com>
2018-12-17 11:22:00 -0800

    setting up dask

--- dask_generate.py
+++ dask_generate.py
@@ -2,36 +2,97 @@ import glob
 from DeepForest import Generate,config
 from dask import compute, delayed
 
-#find training csvs
-DeepForest_config=config.load_config("train")
-data_paths=glob.glob(DeepForest_config['training_csvs']+"/*.csv")
+import subprocess
+import socket
 
-#Local threading/processes, set scheduler.
-#Compute tiles
-values = [delayed(Generate.run)(x) for x in data_paths]
-results = compute(*values, scheduler='processes')
+def find_csvs():
+    """
+    Find training csvs on path
+    """
+    DeepForest_config = config.load_config("train")
+    data_paths = glob.glob(DeepForest_config['training_csvs']+"/*.csv")
+    
+    return data_paths
 
-#from dask.distributed import Client
-client = Client(scheduler_file='scheduler.json')
+def run_local(data_paths):
+    """
+    Run training processes on local laptop
+    """
+    
+    #Local threading/processes, set scheduler.
+    values = [delayed(Generate.run)(x) for x in data_paths]
+    
+    #Compute tiles    
+    results = compute(*values, scheduler='processes')    
+    
+    return results
+    
+### HiperGator ####
 
 #Start juypter notebook to watch
-import socket
-host = client.run_on_scheduler(socket.gethostname)
+def start_tunnel(dask_scheduler):
+    """
+    Start a juypter session and ssh tunnel to view task progress
+    """
+    
+    host = client.run_on_scheduler(socket.gethostname)    
+    proc = subprocess.Popen(['jupyter', 'lab', '--ip', host, '--no-browser'])
+    print("To tunnel into dask dashboard:")
+    print("ssh -N -L 8888:%s:8888 -l b.weinstein hpg2.rc.ufl.edu" % (host))
+    
+def run_HPC(data_paths):
+        
+    #################
+    # Setup dask cluster
+    #################
+    
+    from dask_jobqueue import SLURMCluster
+    from dask.distributed import Client
+    from dask import delayed
+    cluster = SLURMCluster(processes=1,queue='hpg2-compute', threads=2, memory='4GB', walltime='144:00:00')
+    
+    #Load config
+    DeepForest_config = config.load_config("train")
+    
+    print('Starting up workers')
+    workers = []
+    for _ in range(DeepForest_config.num_hipergator_workers):
+        workers.extend(cluster.start_workers(1))
+        sleep(60)
+    dask_client = Client(cluster)
+    
+    wait_time=0
+    while len(dask_client.scheduler_info()['workers']) < DeepForest_config.num_hipergator_workers:
+        print('waiting on workers: {s} sec. so far'.format(s=wait_time))
+        sleep(10)
+        wait_time+=10
+        
+        # If 5 minutes goes by try adding them again
+        if wait_time > 300:
+            workers.extend(cluster.start_workers(1))
+    
+    print('All workers accounted for')
+    
+    #Start dask dashboard? Not clear yet.
+    client.run_on_scheduler(start_tunnel)    
+    #start_tunnel(dask_scheduler)
+    
+    # Local threading/processes, set scheduler.
+    values = [delayed(Generate.run)(x) for x in data_paths]
+    
+    #Compute tiles    
+    results = compute(*values, scheduler='processes')    
+    return results
 
-def start_jlab(dask_scheduler):
-    import subprocess
-    proc = subprocess.Popen(['/path/to/jupyter', 'lab', '--ip', host, '--no-browser'])
-    dask_scheduler.jlab_proc = proc
+if __name__ == "__main__":
+    
+    #Local Debugging
+    data_paths=find_csvs()
 
-client.run_on_scheduler(start_jlab)
-
-results.visualize()
-#Hipergator 
-#Loca
-#
-#import dask.multiprocessing
-#results = compute(*values, scheduler='processes')
-#print(data_paths)
-#results = [Generate.run(x) for x in data_paths]
-
-
+    print("{s} csv files found for training".format(s=len(data_paths)))
+    
+    #run_local(data_paths)
+    
+    #On Hypergator
+    run_HPC(data_paths)
+    
