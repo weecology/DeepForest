1b1d0623da0c56611552d1392b431931697b6fcd
bw4sz <benweinstein2010@gmail.com>
2019-01-24 13:33:32 -0800

    sorry thats retraining

--- HPC/submit_retraining.sh
+++ HPC/submit_retraining.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 stamp=$(date +%Y%m%d_%H%M%S) # time stamp for a directory to catch all logs.
-sbatch --job-name=$stamp.run --output=/home/b.weinstein/logs/$stamp.out --error=/home/b.weinstein/logs/$stamp.err --export=stamp=/home/b.weinstein/logs/$stamp /home/b.weinstein/DeepLidar/HPC/training.sbatch
+sbatch --job-name=$stamp.run --output=/home/b.weinstein/logs/$stamp.out --error=/home/b.weinstein/logs/$stamp.err --export=stamp=/home/b.weinstein/logs/$stamp /home/b.weinstein/DeepLidar/HPC/retraining.sbatch
