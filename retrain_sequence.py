10a30b854eaaaa984e7afde4196a80dcefd62e5e
bw4sz <benweinstein2010@gmail.com>
2019-03-19 10:32:37 -0700

    restting config

--- retrain_sequence.py
+++ retrain_sequence.py
@@ -18,8 +18,8 @@ from eval import parse_args
 
 
 #find models
-#models = glob.glob("/orange/ewhite/b.weinstein/retinanet/20190315_150652/*.h5")
-models = glob.glob("/Users/Ben/Documents/DeepLidar/snapshots/*.h5")
+models = glob.glob("/orange/ewhite/b.weinstein/retinanet/20190315_150652/*.h5")
+#models = glob.glob("/Users/Ben/Documents/DeepLidar/snapshots/*.h5")
 
 #For each model, match the hand annotations with the pretraining model
 results = []
