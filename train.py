1c194708754704a0e218f7d1c8984ab605bbee14
bw4sz <benweinstein2010@gmail.com>
2019-04-03 11:45:38 -0700

    back to just RGB

--- train.py
+++ train.py
@@ -503,7 +503,7 @@ if __name__ == '__main__':
             
         #pass an args object instead of using command line    
         args = [
-            "--epochs", str(25),
+            "--epochs", str(50),
             "--batch-size", str(DeepForest_config['batch_size']),
             "--backbone", str(DeepForest_config["backbone"]),
             "--score-threshold", str(DeepForest_config["score_threshold"]),
