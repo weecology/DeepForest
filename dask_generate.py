41765029853d960d0e42a4a305a88a6f2d3b89d4
bw4sz <benweinstein2010@gmail.com>
2019-04-05 23:05:34 -0400

    reset

--- dask_generate.py
+++ dask_generate.py
@@ -91,7 +91,7 @@ def run_HPC(data_paths):
         processes=1,
         queue='hpg2-compute',
         cores=1, 
-        memory='22GB', 
+        memory='20GB', 
         walltime='48:00:00',
         job_extra=extra_args,
         local_directory="/home/b.weinstein/logs/", death_timeout=300)
@@ -119,7 +119,7 @@ if __name__ == "__main__":
     print("{s} csv files found for training".format(s=sum(total_files)))
     
     #run_local(data_paths)
-    run_test(data_paths)
+    #run_test(data_paths)
     
     #On Hypergator
-    #run_HPC(data_paths)
+    run_HPC(data_paths)
