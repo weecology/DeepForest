0fc1f135b390f75609eb6f468d2ad8c7699cb520
bw4sz <benweinstein2010@gmail.com>
2019-04-06 09:39:54 -0700

    resize when placing in h5 object

--- DeepForest/Generate.py
+++ DeepForest/Generate.py
@@ -123,7 +123,7 @@ def run(tile_csv=None, tile_xml = None, mode="train", site=None):
         image = generator.load_image(i)
         
         #resize image
-        image = generator.resize_image(image)
+        image, scale = generator.resize_image(image)
         
         #If image window is corrupt (RGB missing), go to next tile, it won't be in labeldf
         if image is None:
