9a9019e59c0d08e6ea02ebcd0f920c57bfa0049d
bw4sz <benweinstein2010@gmail.com>
2019-04-06 14:09:00 -0700

    not allowed to log code

--- train.py
+++ train.py
@@ -417,7 +417,7 @@ if __name__ == '__main__':
     DeepForest_config = load_config()
 
     #set experiment and log configs
-    experiment = Experiment(api_key="ypQZhYfs3nSyKzOfz13iuJpj2", project_name='deeplidar')
+    experiment = Experiment(api_key="ypQZhYfs3nSyKzOfz13iuJpj2", project_name='deeplidar',log_code=False)
 
     #save time for logging
     if mode.dir:
@@ -514,7 +514,7 @@ if __name__ == '__main__':
         ]
     
         DeepForest_config["evaluation_images"] = 0 
-        experiment = Experiment(api_key="ypQZhYfs3nSyKzOfz13iuJpj2", project_name='deeplidar')
+        experiment = Experiment(api_key="ypQZhYfs3nSyKzOfz13iuJpj2", project_name='deeplidar',log_code=False)
         experiment.log_parameters(DeepForest_config)    
         experiment.log_parameter("Start Time", dirname)    
         experiment.log_parameter("mode", "final_hand_annotation")
