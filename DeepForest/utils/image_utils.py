993073cf8a087803cd2edef1fd103b9cfe1527e5
bw4sz <benweinstein2010@gmail.com>
2019-04-05 19:37:39 -0700

    no norm

--- DeepForest/utils/image_utils.py
+++ DeepForest/utils/image_utils.py
@@ -28,8 +28,7 @@ def normalize_four_channel(image):
     
     """Normalize array inputs by max value. Threshold used for height"""
     
-    image[:,:,0] =  image[:,:,0]/image[:,:,0]/10
-    image[:,:,1] =  image[:,:,1]/image[:,:,0]/10
+    image[:,:,0] =  image[:,:,0]/image[:,:,0]
     
     return image
 
