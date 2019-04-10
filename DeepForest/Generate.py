6e6d46733d012541f478a0021ed7fa5997bcc65c
bw4sz <benweinstein2010@gmail.com>
2019-04-10 15:07:12 -0700

    expand dim

--- DeepForest/Generate.py
+++ DeepForest/Generate.py
@@ -146,6 +146,8 @@ def run(tile_csv=None, tile_xml = None, mode="train", site=None):
         #TODO do boxes need to be scaled in some way?    
         image, scale = generator.resize_image(image)
         
+        iamge = np.expand_dims(image, 2)
+        
         hdf5_file["train_imgs"][i,...] = image        
         
         #Load annotations and write a pandas frame
