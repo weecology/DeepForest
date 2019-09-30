c961914c5ffde17697e7b72fe15ad30a8c24a239
bw4sz <benweinstein2010@gmail.com>
2019-09-30 10:13:21 -0700

    setting up tests and env

--- deepforest/deepforest.py
+++ deepforest/deepforest.py
@@ -1,3 +1,22 @@
-# -*- coding: utf-8 -*-
+"""
+.. module:: deepforest
+   :platform: Unix, Windows
+   :synopsis: A module for individual tree crown detection using deep learning neural networks. see Weinstein et al. Remote Sensing. 2019
 
-"""Main module."""
+.. moduleauthor:: Ben Weinstein <ben.weinstein@weecology.org>
+
+"""
+
+class deepforest:
+    """Overall Class for training and predicting images
+    """
+    
+    def __init__(self, weights=None):
+        """A deepforest object for model training or prediction"""
+        self.weights = weights
+    
+    def train(self):
+        """A training model"""
+        
+    def prediction(self):
+        """A prediction model"""
--- tests/test_deepforest.py
+++ tests/test_deepforest.py
@@ -3,23 +3,32 @@
 
 """Tests for `deepforest` package."""
 
+#Path hack
+import os
+import sys
+dir_path = os.path.dirname(os.path.realpath(__file__))
+parent_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+sys.path.append(parent_path)
+
 import pytest
-
-
 from deepforest import deepforest
 
+def test_deepforest():
+    model = deepforest.deepforest()
+    assert model.weights is None
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+test_deepforest()
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+#@pytest.fixture
+#def response():
+    #"""Sample pytest fixture.
+
+    #See more at: http://doc.pytest.org/en/latest/fixture.html
+    #"""
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+#def test_content(response):
+    #"""Sample pytest test function with the pytest fixture as an argument."""
+    ## from bs4 import BeautifulSoup
+    ## assert 'GitHub' in BeautifulSoup(response.content).title.string
