bf614a1edcc8dc0bdc853121e323903ba2520665
bw4sz <benweinstein2010@gmail.com>
2019-09-30 11:28:28 -0700

    docs build

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+from recommonmark.parser import CommonMarkParser
 
 import deepforest
 
@@ -40,8 +41,10 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+source_parsers = {
+    '.md': CommonMarkParser,
+}
 
 # The master toctree document.
 master_doc = 'index'
