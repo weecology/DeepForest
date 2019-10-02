ef278b808098031704191712c0a6eee4a1aec422
bw4sz <benweinstein2010@gmail.com>
2019-10-02 15:06:08 -0700

    added a prediction fixture

--- tests/test_deepforest.py
+++ tests/test_deepforest.py
@@ -9,11 +9,17 @@ import keras
 from  deepforest import deepforest
 from deepforest import utilities
 
+#download latest release
 @pytest.fixture()
 def download_release(scope="session"):
     print("running fixtures")
     utilities.use_release()    
     
+@pytest.fixture()
+def prediction_model(download_release):
+    test_model = deepforest.deepforest(weights="data/universal_model_july30.h5")
+    return test_model
+
 def test_deepforest():
     model = deepforest.deepforest(weights=None)
     assert model.weights is None
@@ -24,6 +30,6 @@ def test_use_release(download_release):
     #Assert is model instance    
     assert isinstance(test_model.model,keras.models.Model)
 
-def test_predict(download_release):
-    test_model = deepforest.deepforest(weights="data/universal_model_july30.h5")
-    
+def test_predict_image(prediction_model):
+    #Test fixture
+    assert isinstance(prediction_model.model,keras.models.Model)
