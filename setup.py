9da56f3e2fa7aace23e2903d40245d7ab844c229
bw4sz <benweinstein2010@gmail.com>
2019-10-08 10:47:43 -0700

    add test image, rewrite setup.py

--- setup.py
+++ setup.py
@@ -1,55 +1,51 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""The setup script."""
-
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
+NAME ='deepforest'
+VERSION = '0.1.0'
+DESCRIPTION = 'Plant phenology models in python'
+URL = 'https://github.com/Weecology/DeepForest'
+AUTHOR = 'Ben Weinstein'
+LICENCE = 'MIT'
+LONG_DESCRIPTION = """
+# Deepforest
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+## Full documentation  
+[http://deepforest.readthedocs.io/en/latest/](http://deepforest.readthedocs.io/en/latest/)
+## Installation
 
-requirements = [ ]
+conda create --file=environment.yml
 
-setup_requirements = ['pytest-runner', ]
+```
+Or install the latest version from Github  
+```
+pip install git+git://github.com/Weecology/deepforest
+```
 
-test_requirements = ['pytest>=3', ]
+## Get in touch
+See the [GitHub Repo](https://github.com/Weecology/deepforest) to see the 
+source code or submit issues and feature requests.
 
-setup(
-    author="Ben Weinstein",
-    author_email='ben.weinstein@weecology.org',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-    description="Python package for individual tree crown prediction in airborne imagery",
-    entry_points={
-        'console_scripts': [
-            'deepforest=deepforest.cli:main',
-        ],
-    },
-    install_requires=requirements,
-    license="MIT license",
-    long_description=readme + '\n\n' + history,
-    include_package_data=True,
-    keywords='deepforest',
-    name='deepforest',
-    packages=find_packages(include=['deepforest', 'deepforest.*']),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
-    url='https://github.com/bw4sz/deepforest',
-    version='0.1.0',
-    zip_safe=False,
-)
+## Citation
+If you use this software in your research please cite it as:
+
+Geographic Generalization in Airborne RGB Deep Learning Tree Detection
+Ben. G. Weinstein, Sergio Marconi, Stephanie A. Bohlman, Alina Zare, Ethan P. White
+bioRxiv 790071; doi: https://doi.org/10.1101/790071
+
+
+## Acknowledgments
+Development of this software was funded by
+[the Gordon and Betty Moore Foundation's Data-Driven Discovery Initiative](http://www.moore.org/programs/science/data-driven-discovery) through
+[Grant GBMF4563](http://www.moore.org/grants/list/GBMF4563) to Ethan P. White.
+"""
+setup(name=NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description = LONG_DESCRIPTION,
+      long_description_content_type='text/markdown',
+      url=URL,
+      author=AUTHOR,
+      license=LICENCE,
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False)
