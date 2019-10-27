cda48d21f03e3b2732ecc485ef84b0f26d002f0a
bw4sz <benweinstein2010@gmail.com>
2019-10-26 20:35:25 -0700

    tf records doesn't train yet, reshape error, but better testing env

--- deepforest/tfrecords.py
+++ deepforest/tfrecords.py
@@ -1,14 +1,16 @@
-from keras_retinanet import models
 import tensorflow as tf
 import numpy as np
 from math import ceil
+import keras 
 from keras_retinanet.preprocessing.csv_generator import CSVGenerator
+from keras_retinanet import models
+from keras_retinanet.models.retinanet import retinanet_bbox
+from keras_retinanet import losses
 
-def create_tf_example(image, regression_target, class_target, filename):
+def create_tf_example(image, regression_target, class_target):
     #Image data 
     height = image.shape[0]
     width = image.shape[1]
-    filename = filename
     image_format = b'jpg'
     classes_text = ['Tree']
     classes = [1]
@@ -18,22 +20,24 @@ def create_tf_example(image, regression_target, class_target, filename):
         'image/width': tf.train.Feature(int64_list=tf.train.Int64List(value=[image.shape[1]])),
         'image/encoded':  tf.train.Feature(bytes_list=tf.train.BytesList(value=[image.tobytes()])),
         'image/n_anchors': tf.train.Feature(int64_list=tf.train.Int64List(value=[regression_target.shape[0]])),         
-        'image/object/regression_target': tf.train.Feature(float_list=tf.train.FloatList(value=regression_target.flatten())),
-        'image/object/class_target': tf.train.Feature(float_list=tf.train.FloatList(value=class_target.flatten())),
+        'image/object/regression_target': tf.train.Feature(bytes_list=tf.train.BytesList(value=[regression_target.tobytes()])),
+        'image/object/class_target': tf.train.Feature(bytes_list=tf.train.BytesList(value=[class_target.tobytes()])),
     }))
     
     # Serialize to string and write to file
     return example
 
-def create_tfrecords(backbone_model, image_min_side, size, savedir):
+#TODO create classes file from annotations_file
+def create_tfrecords(annotations_file, class_file, backbone_model="resnet50", image_min_side=800, size=1, savedir="."):
     """
     Args:
-        backbone:
-        image_min_side:
-        size:
-        savedir:
+        annotations_file: path to 5 column data in form image_path, xmin, ymin, xmax, ymax, label
+        backbone_model: A keras retinanet backbone
+        image_min_side: resized image object minimum size
+        size: Number of images per tfrecord
+        savedir: dir path to save tfrecords files
     Returns:
-        NULL -> side effect
+        NULL -> side effect writes tfrecords
     """
     #Image preprocess function
     backbone = models.backbone(backbone_model)
@@ -56,11 +60,10 @@ def create_tfrecords(backbone_model, image_min_side, size, savedir):
     
     for chunk in chunks:
         #Create tfrecord dataset
-        writer = tf.python_io.TFRecordWriter(savedir + "{}.tfrecords".format(chunk[0]))
+        writer = tf.python_io.TFRecordWriter(savedir + "{}.tfrecord".format(chunk[0]))
         images = []
         regression_targets = []
         class_targets = []
-        filenames = []
         
         for i in chunk:
             batch = train_generator.__getitem__(i),
@@ -68,8 +71,8 @@ def create_tfrecords(backbone_model, image_min_side, size, savedir):
             #split into images and targets
             inputs, targets =  batch[0]
            
-            #grab image, asssume batch size of 1
-            images.append(inputs)
+            #grab image, asssume batch size of 1, squeeze
+            images.append(inputs[0,...])
             
             #Grab anchor targets
             regression_batch, labels_batch = targets
@@ -78,19 +81,20 @@ def create_tfrecords(backbone_model, image_min_side, size, savedir):
             #regression_batch: batch that contains bounding-box regression targets for an image & anchor states (np.array of shape (batch_size, N, 4 + 1),
             #where N is the number of anchors for an image, the first 4 columns define regression targets for (x1, y1, x2, y2) and the
             #last column defines anchor states (-1 for ignore, 0 for bg, 1 for fg).       
-            regression_anchors = regression_batch     
+            regression_anchors = regression_batch[0,...] 
             regression_targets.append(regression_anchors)
             
-            #grab class labels
+            #grab class labels - squeeze out batch size
             #From retinanet: labels_batch: batch that contains labels & anchor states (np.array of shape (batch_size, N, num_classes + 1),
            #where N is the number of anchors for an image and the last column defines the anchor state (-1 for ignore, 0 for bg, 1 for fg).        
-            labels = labels_batch
+            labels = labels_batch[0,...]
+            print("Label shape is: {}".format(labels.shape))
             class_targets.append(labels)
             
             i +=1
                 
-        for image, regression_target, class_target, filename in zip(images,regression_targets, class_targets, filenames):
-            tf_example = create_tf_example(image, regression_target, class_target, filename)
+        for image, regression_target, class_target in zip(images,regression_targets, class_targets):
+            tf_example = create_tf_example(image, regression_target, class_target)
             writer.write(tf_example.SerializeToString())        
             
 
@@ -103,24 +107,32 @@ def _parse_fn(example):
         "image/width": tf.io.FixedLenFeature([], tf.int64),
         'image/encoded': tf.io.FixedLenFeature([], tf.string),       
         "image/n_anchors": tf.io.FixedLenFeature([], tf.int64),
-        "image/object/regression_target": tf.FixedLenFeature([], tf.float32),
-        "image/object/class_target": tf.FixedLenFeature([], tf.float32)
+        "image/object/regression_target": tf.FixedLenFeature([], tf.string),
+        "image/object/class_target": tf.FixedLenFeature([], tf.string)
                         }
     
     # Load one example and parse
     example = tf.io.parse_single_example(example, features)
-    image = tf.decode_raw(example['image/encoded'], tf.uint8)
+    image = tf.decode_raw(example['image/encoded'], tf.float32)
     height = tf.cast(example['image/height'], tf.int32)
     width = tf.cast(example['image/width'], tf.int32)
     n_anchors = tf.cast(example['image/n_anchors'], tf.int32)
-    regression_target = tf.cast(example['image/object/regression_target'], tf.float32)
-    class_target = tf.cast(example['image/object/class_target'], tf.float32)
+    regression_target = tf.decode_raw(example['image/object/regression_target'], tf.float32)
+    class_target = tf.decode_raw(example['image/object/class_target'], tf.float32)
+    
+    #TODO allow this vary from config? Or read during sess?    
+    image = tf.reshape(image, [800, 800, 3],name="cast_image")            
+    regression_target = tf.reshape(regression_target, [120087, 5], name="cast_regression")
+    class_target = tf.reshape(class_target, [120087, 3], name="cast_class_label")
     
     return image, regression_target, class_target
 
 def create_dataset(filepath):
     """
-    filepath: list of tfrecord files
+    Args:
+        filepath: list of tfrecord files
+    Returns:
+        dataset: a tensorflow dataset object for model training or prediction
     """
     
     # This works with arrays as well
@@ -136,29 +148,44 @@ def create_dataset(filepath):
     #dataset = dataset.shuffle(1000)
     
     ## Set the batchsize
-    dataset = dataset.batch(1)
+    dataset = dataset.batch(1, drop_remainder=True)
     
     ## Create an iterator
     iterator = dataset.make_one_shot_iterator()
     
+    return iterator
+
     ## Create your tf representation of the iterator
-    image, regression_target, class_target = iterator.get_next()
-    
-    #TODO allow this vary from config? Or read during sess?
-    image = tf.reshape(image, [-1, 800, 800, 3])    
-    
-    regression_target = tf.reshape(regression_target, [-1, 120087, 5])
-    class_target = tf.reshape(class_target, [-1, 120087, 2])
+    #image, regression_target, class_target = iterator.get_next()
     
     #stack regression and class targets?    
-    return image, [regression_target, class_target]
+    #return image, [regression_target, class_target]
 
-def train(path_to_tfrecord, steps_per_epoch=None):
+def train(path_to_tfrecord, backbone_model, steps_per_epoch=None):
+    """
+    Train a retinanet model using tfrecords input
+    
+    Args:
+        path_to_tfrecord: a path or wildcard glob of tfrecords
+        backbone_model: A keras retinanet backbone
+        steps_per_epoch: How often should validation data be evaluated?
+    
+    Returns:
+        training_model: The retinanet training model
+        prediction_model: The retinanet prediction model with nms for bbox
+    """
     
     #Create tensorflow iterator
-    inputs, targets = create_dataset(path_to_tfrecord)
+    iterator = create_dataset(path_to_tfrecord)
+    next_element = iterator.get_next()
     
-    #Create model
+    #Split into inputs and targets 
+    inputs = next_element[0]
+    targets = [next_element[1], next_element[2]]
+    
+    #Create model 
+    #TODO how to mimic generator inputs?
+    backbone = models.backbone(backbone_model)    
     model = backbone.retinanet(num_classes=1)
     training_model = model
     
@@ -178,6 +205,7 @@ def train(path_to_tfrecord, steps_per_epoch=None):
     #Train model
     if steps_per_epoch is None:
         raise ValueError("Unknown steps for a tfrecord")
-    training_model.fit(steps_per_epoch=steps_per_epoch)
+    
+    training_model.fit(inputs, steps_per_epoch=steps_per_epoch)
     
     return training_model, prediction_model    
--- tests/test_tfrecords.py
+++ tests/test_tfrecords.py
@@ -1,6 +1,10 @@
 #test module for tfrecords
 from deepforest import tfrecords
+from deepforest import utilities
+from deepforest import preprocess
+
 import pytest
+import os
 
 @pytest.fixture("module")
 def config():
@@ -11,27 +15,29 @@ def config():
     config["rgb_dir"] = "tests/data"
     config["annotations_file"] = "tests/data/OSBS_029.csv"
     config["path_to_raster"] ="tests/data/OSBS_029.tif"
+    config["image-min-side"] = 800
+    config["backbone"] = "resnet50"
     
     #Create a clean config test data
     annotations = utilities.xml_to_annotations(xml_path=config["annotations_xml"],rgb_dir= config["rgb_dir"])
     annotations.to_csv("tests/data/OSBS_029.csv",index=False)
     annotations_file = preprocess.split_training_raster(config["path_to_raster"], config["annotations_file"], "tests/data/",config["patch_size"], config["patch_overlap"])
+    annotations_file.to_csv("tests/data/OSBS_029_crop.csv", index=False,header=False)
     return config
 
 @pytest.fixture()
-def prepare_dataset(config):
-    tfrecords.create_tfrecords("tests/data/OSBS_029.csv", "tests/data/classes.csv", config["image_min_side"], config["backbone"], size=1)
+def prepare_dataset(config):    
+    tfrecords.create_tfrecords(annotations_file="tests/data/OSBS_029_crop.csv", class_file="tests/data/classes.csv", image_min_side=config["image-min-side"], backbone_model=config["backbone"], size=10, savedir="tests/data/")
+    assert os.path.exists("tests/data/0.tfrecord")
 
 #Writing
 def test_create_tfrecords(config):
-    tfrecords.create_tfrecords(annotations_file, class_file, image_min_side, backbone, size)
+    tfrecords.create_tfrecords(annotations_file="tests/data/OSBS_029_crop.csv", class_file="tests/data/classes.csv", image_min_side=config["image-min-side"], backbone_model=config["backbone"], size=10)
 
 #Reading
-def test_create_dataset():
-    dataset = tfrecords.create_dataset("tests/data/OSBS_029.tfrecords")
-
+def test_create_dataset(prepare_dataset):
+    dataset = tfrecords.create_dataset("tests/data/0.tfrecord")        
+        
 #Training    
-def test_train(prepare_dataset):
-    training_model, prediction_model = tfrecords.train("tests/data/OSBS_029.tfrecords")
-    
-    
+def test_train(prepare_dataset, config):
+    tfrecords.train(path_to_tfrecord="tests/data/0.tfrecord", steps_per_epoch = 10, backbone_model=config["backbone"])
