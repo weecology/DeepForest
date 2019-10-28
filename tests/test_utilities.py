e99dec47676a9876524724b7d04dc96c1f0917e0
bw4sz <benweinstein2010@gmail.com>
2019-10-28 10:23:52 -0700

    allow utilities to override steps per epoch

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -3,12 +3,20 @@ from deepforest import utilities
 import pytest
 import os
 import pandas as pd
+import numpy as np
 
 @pytest.fixture()
 def annotations():
     annotations = utilities.xml_to_annotations("tests/data/OSBS_029.xml",rgb_dir="tests/data")
+    annotations_file = "tests/data/OSBS_029.csv"
     annotations.to_csv("tests/data/OSBS_029.csv",index=False)
+    return annotations_file
 
+@pytest.fixture()
+def config():
+    config  = utilities.read_config()
+    return config
+    
 def test_xml_to_annotations():
     annotations = utilities.xml_to_annotations(xml_path = "tests/data/OSBS_029.xml",rgb_dir = "tests/data")
     print(annotations.shape)
@@ -18,14 +26,21 @@ def test_xml_to_annotations():
     assert annotations["xmin"].dtype == "int"
     
 def test_create_classes(annotations):
-    classes_file = utilities.create_classes(annotations_file="tests/data/OSBS_029.csv")
+    classes_file = utilities.create_classes(annotations_file=annotations)
     assert os.path.exists(classes_file)
 
 def test_number_of_images(annotations):
-    n = utilities.number_of_images(annotations_file="tests/data/OSBS_029.csv")
+    n = utilities.number_of_images(annotations_file=annotations)
     assert n == 2
     
-def format_args():
-    config  = utilities.read_config()
+def test_format_args(annotations, config):
     arg_list = utilities.format_args(annotations, config)
     assert isinstance(arg_list, list)
+
+def test_format_args_steps(annotations, config):
+    arg_list = utilities.format_args(annotations, config, steps_per_epoch=100)
+    assert isinstance(arg_list, list)
+    
+    #A bit ugly, but since its a list, what is the argument after --steps to assert
+    steps_position = np.where(["--steps" in x for x in arg_list])[0][0] + 1
+    assert arg_list[steps_position] == '100'
