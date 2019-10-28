a1c4b8f41f47e65fd6a445e7b242cfe14eef22fe
bw4sz <benweinstein2010@gmail.com>
2019-10-28 14:27:00 -0700

    variable basenames

--- deepforest/utilities.py
+++ deepforest/utilities.py
@@ -141,7 +141,7 @@ def create_classes(annotations_file):
         n_classes = labels.shape[0]
         print("There are {} unique labels: {} ".format(n_classes,list(labels))) 
         if n_classes > 1:
-                warnings.warn("There are greater than 1 classes, check annotation levels for file {}".format(annotations_file))
+                raise ValueError("There are greater than 1 classes ({}), check annotation levels for file {}".format(list(labels), annotations_file))
         
         #write label
         with open(classes_path,'w') as csv_file:
