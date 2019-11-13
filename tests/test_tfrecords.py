5e4a634dc200f224b86f9a8df044844e3974510c
bw4sz <benweinstein2010@gmail.com>
2019-11-13 10:58:29 -0800

    mystery travis error

--- tests/test_tfrecords.py
+++ tests/test_tfrecords.py
@@ -15,6 +15,7 @@ from keras_retinanet import models
 
 @pytest.fixture()
 def config():
+    print("Configuring tfrecord tests")
     config = {}
     config["patch_size"] = 200
     config["patch_overlap"] = 0.05
@@ -38,12 +39,10 @@ def config():
     annotations_file.to_csv("tests/data/testfile_tfrecords.csv", index=False,header=False)
     return config
 
-@pytest.fixture()
-def prepare_dataset(config):    
-    tfrecords.create_tfrecords(annotations_file="tests/data/testfile_tfrecords.csv", class_file="tests/data/classes.csv", image_min_side=config["image-min-side"], backbone_model=config["backbone"], size=10, savedir="tests/data/")
-    assert os.path.exists("tests/data/testfile_tfrecords_0.tfrecord")
+#Reading
 
 #Writing
+@pytest.fixture()
 def test_create_tfrecords(config):
     """This test is in flux due to the fact that tensorflow and cv2 resize methods are not identical: https://jricheimer.github.io/tensorflow/2019/02/11/resize-confusion/ """
     created_records = tfrecords.create_tfrecords(annotations_file="tests/data/testfile_tfrecords.csv",
@@ -53,10 +52,28 @@ def test_create_tfrecords(config):
                                size=100,
                                savedir="tests/data/")
     assert os.path.exists("tests/data/testfile_tfrecords_0.tfrecord")
+    return created_records
+
+@pytest.fixture()
+def test_create_tensors(test_create_tfrecords):
+    print("Testing that input tensors can be created")
+    created_tensors = tfrecords.create_tensors(test_create_tfrecords)
+    assert len(created_tensors) == 2
+    
+    return created_tensors
+
+#@pytest.mark.skipif("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", reason="tfrecords can't build on travis")
+def test_create_dataset(test_create_tfrecords):
+    dataset = tfrecords.create_dataset(test_create_tfrecords)
+    
+def test_equivalence(config, test_create_tensors):
+    
+    #unpack created tensors
+    tf_inputs, tf_targets = test_create_tensors
     
     #the image going in to tensorflow should be equivalent to the image from the fit_generator
     backbone = models.backbone(config["backbone"])
-        
+
     #CSV generator
     generator = csv_generator.CSVGenerator(
         csv_data_file="tests/data/testfile_tfrecords.csv",
@@ -64,18 +81,16 @@ def test_create_tfrecords(config):
         image_min_side=config["image-min-side"],
         preprocess_image=backbone.preprocess_image,
     )
-    
+
     #find file in randomize generator group
     first_file = generator.groups[0][0]
     gen_filename = os.path.join(generator.base_dir, generator.image_names[first_file])
     original_image = generator.load_image(first_file)
     inputs, targets = generator.__getitem__(0)
-    
+
     image = inputs[0,...]
     targets = targets[0][0,...]
-    
-    tf_inputs, tf_targets = tfrecords.create_tensors(created_records, shuffle = False)
-    
+
     with tf.Session() as sess:
         #seek the randomized image to match
         tf_inputs, tf_targets = sess.run([tf_inputs,tf_targets])
@@ -85,19 +100,19 @@ def test_create_tfrecords(config):
     #tf_image = tf_image[0,...]
     tf_inputs = tf_inputs[0,...]
     tf_targets = tf_targets[0][0,...]
-    
+
     #Same shape
     #assert tf_image.shape == image.shape
     assert tf_inputs.shape == image.shape
     assert tf_targets.shape == targets.shape
-    
+
     #Same values, slightly duplicitious with above, but useful for debugging.
     #Saved array is the same as generator image
     #np.testing.assert_array_equal(image, tf_image)
-    
+
     #Loaded array is the same as generator, this is not true currently, the opencv and the tensorflow interpolation method is slightly different, waiting for tf. 2.0
-   #np.testing.assert_array_equal(tf_loaded, tf_image)
-    
+    #np.testing.assert_array_equal(tf_loaded, tf_image)
+
     ##Useful for debug to plot
     #fig = plt.figure()
     #ax1 = fig.add_subplot(1,4,1)
@@ -110,8 +125,5 @@ def test_create_tfrecords(config):
     #ax2 = fig.add_subplot(1,4,4)
     #ax2.title.set_text('Loaded Image')        
     #plt.imshow(tf_inputs)
-    #plt.show()    
+    #plt.show()        
     
-#Reading
-def test_create_dataset(prepare_dataset):
-    dataset = tfrecords.create_dataset("tests/data/testfile_tfrecords_0.tfrecord")
