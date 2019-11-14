b74565d33aeed167eb47f8cae0e4fb2a76f641eb
bw4sz <benweinstein2010@gmail.com>
2019-11-14 09:43:53 -0800

    testing set trainable to false for all layers

--- deepforest/retinanet_train.py
+++ deepforest/retinanet_train.py
@@ -104,7 +104,8 @@ def create_models(backbone_retinanet, num_classes, weights, multi_gpu=0,
         #for layer in training_model.layers[:freeze_layers]:
             #print("Freezing layer {}".format(layer.name))
             #layer.trainable = False
-    training_model.trainable = False
+    for layer in training_model.layers[:-4]:
+        layer.trainable=False
         
     #Compile model
     if targets:
