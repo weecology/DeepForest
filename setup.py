f5da86c221dbe541afc9c1f681c34f886a0eca99
bw4sz <benweinstein2010@gmail.com>
2019-11-26 11:48:15 -0800

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 NAME ='deepforest'
-VERSION = '0.1.2'
+VERSION = '0.1.3'
 DESCRIPTION = 'Tree Crown Prediction using Deep Learning Retinanets'
 URL = 'https://github.com/Weecology/DeepForest'
 AUTHOR = 'Ben Weinstein'
