5c10987619ca4c752b280f914e87868246905559
bw4sz <benweinstein2010@gmail.com>
2020-01-06 14:25:29 -0800

    working on retinanet install

--- setup.py
+++ setup.py
@@ -60,5 +60,5 @@ setup(name=NAME,
       license=LICENCE,
       packages=find_packages(),
       include_package_data=True,
-      install_requires=["keras > 2.3.0","tensorflow==1.14","pillow","pandas","opencv-python","pyyaml","slidingwindow","matplotlib","xmltodict","tqdm","numpy"],
+      install_requires=["keras > 2.3.0","tensorflow==1.15","pillow","pandas","opencv-python","pyyaml","slidingwindow","matplotlib","xmltodict","tqdm","numpy","Cython"],
       zip_safe=False)
