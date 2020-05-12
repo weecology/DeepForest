bcdc6c976740500a76ef70e3d39c6ca5829febf8
Henry Kironde <henrykironde@gmail.com>
2020-05-12 19:50:49 -0400

    keras AnchorParameters.default = None

--- keras_retinanet/utils/anchors.py
+++ keras_retinanet/utils/anchors.py
@@ -42,12 +42,16 @@ class AnchorParameters:
 """
 The default anchor parameters.
 """
-AnchorParameters.default = AnchorParameters(
-    sizes   = [32, 64, 128, 256, 512],
-    strides = [8, 16, 32, 64, 128],
-    ratios  = np.array([0.5, 1, 2], keras.backend.floatx()),
-    scales  = np.array([2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)], keras.backend.floatx()),
-)
+AnchorParameters.default = None
+try:
+    AnchorParameters.default = AnchorParameters(
+        sizes   = [32, 64, 128, 256, 512],
+        strides = [8, 16, 32, 64, 128],
+        ratios  = np.array([0.5, 1, 2], keras.backend.floatx()),
+        scales  = np.array([2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)], keras.backend.floatx()),
+    )
+except:
+    pass
 
 
 def anchor_targets_bbox(
