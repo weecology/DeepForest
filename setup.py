e8008613e5cf3f471a82593314c467a19a2276a5
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-02-06 19:21:10 -0800

    worried about the hypen in the package name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import setuptools
 from distutils.command.build_ext import build_ext as DistUtilsBuildExt
 
-NAME = 'deepforest-pytorch'
+NAME = 'deepforest_pytorch'
 VERSION = '0.0.1'
 DESCRIPTION = 'Tree crown prediction using deep learning retinanets'
 URL = 'https://github.com/Weecology/DeepForest-pytorch'
