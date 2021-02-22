78d910e047ffa7d6c8bbc7670b8fa8e3be60e2e1
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-02-21 17:43:22 -0800

    log val loss

--- deepforest/main.py
+++ deepforest/main.py
@@ -197,11 +197,13 @@ class deepforest(pl.LightningModule):
         images, targets = batch
         
         self.backbone.train()
-        
         loss_dict = self.backbone.forward(images, targets)
         
         #sum of regression and classification loss
         losses = sum([loss for loss in loss_dict.values()])
+        
+        #Log loss
+        self.log('val_loss', loss_dict, on_epoch=True)
         
         return losses
     
