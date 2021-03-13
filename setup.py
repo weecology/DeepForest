47370fd71f74425a3186a38b5a0ee91056e13de5
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-03-12 18:16:10 -0800

    pin pillow build

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name=NAME,
       packages=find_packages(),
       include_package_data=True,
       install_requires=[
-          "torch", "torchvision", "matplotlib", "Pillow", "pandas", "progressbar2", "six", "scipy>1.5", "slidingwindow","scikit-image","geopandas",'rasterio','rtree',"pytorch_lightning",
+          "torch", "torchvision", "matplotlib", "Pillow>6.2.0", "pandas", "progressbar2", "six", "scipy>1.5", "slidingwindow","scikit-image","geopandas",'rasterio','rtree',"pytorch_lightning",
           "tqdm", "xmltodict", "numpy"
       ],
       zip_safe=False)
