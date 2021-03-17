f36ac1f3ffa187c2e268364b689504f6bddc1c2a
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-03-17 09:38:08 -0700

    update manifest to include package data for #8

--- tests/test_data.py
+++ tests/test_data.py
@@ -7,6 +7,10 @@ import deepforest
 # Make sure package data is present
 def test_get_data():
     assert os.path.exists(deepforest.get_data("testfile_deepforest.csv"))
+    assert os.path.exists(deepforest.get_data("testfile_multi.csv"))    
+    assert os.path.exists(deepforest.get_data("example.csv")) 
+    assert os.path.exists(deepforest.get_data("2019_YELL_2_541000_4977000_image_crop.png"))
     assert os.path.exists(deepforest.get_data("OSBS_029.png"))
     assert os.path.exists(deepforest.get_data("OSBS_029.tif"))
+    assert os.path.exists(deepforest.get_data("SOAP_061.png"))    
     assert os.path.exists(deepforest.get_data("classes.csv"))
