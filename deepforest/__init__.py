882a5ec0f9cd3a19c434c410d415282e3f0981d7
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-05-17 06:45:25 -0700

    Bump version: 0.1.33 → 0.1.33

--- deepforest/__init__.py
+++ deepforest/__init__.py
@@ -2,7 +2,7 @@
 """Top-level package for DeepForest."""
 __author__ = """Ben Weinstein"""
 __email__ = 'ben.weinstein@weecology.org'
-__version__ = '0.1.33'
+__version__ = '__version__ = '0.1.33''
 
 import os
 
