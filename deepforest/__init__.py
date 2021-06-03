4b677d0bddc1437d2eb60305b47405c19d1352f9
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-06-03 11:47:25 -0700

    bumpversion malformed the version number

--- deepforest/__init__.py
+++ deepforest/__init__.py
@@ -2,7 +2,7 @@
 """Top-level package for DeepForest."""
 __author__ = """Ben Weinstein"""
 __email__ = 'ben.weinstein@weecology.org'
-__version__ = '__version__ = '0.2.4''
+__version__ = '0.2.4'
 
 import os
 
