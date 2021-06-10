d8fc0d54747f90acdd3020aa954c1fb159e69180
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-06-10 13:38:58 -0700

    Bump version: 1.0.4 → 1.0.5

--- deepforest/__init__.py
+++ deepforest/__init__.py
@@ -2,7 +2,7 @@
 """Top-level package for DeepForest."""
 __author__ = """Ben Weinstein"""
 __email__ = 'ben.weinstein@weecology.org'
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 import os
 
--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 project = u'DeepForest'
 copyright = u"2019, Ben Weinstein"
 author = u"Ben Weinstein"
-version = u"__version__ = '__version__ = '__version__ = '1.0.4'''"
+version = u"__version__ = '__version__ = '__version__ = '1.0.5'''"
 language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 pygments_style = 'sphinx'
