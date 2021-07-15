bc2f750890dc049d5b7b4c7d2044f0a58b81035e
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-07-14 19:29:45 -0700

    Bump version: 1.0.8 → 1.0.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 from distutils.command.build_ext import build_ext as DistUtilsBuildExt
 
 NAME = 'deepforest'
-VERSION = '1.0.8'
+VERSION = '1.0.9'
 DESCRIPTION = 'Tree crown prediction using deep learning retinanets'
 URL = 'https://github.com/Weecology/DeepForest'
 AUTHOR = 'Ben Weinstein'
