452e49c30ba2ad5440246fa764dab9a99fd3eda1
Ethan White <ethan@weecology.org>
2023-04-23 08:40:05 -0400

    Move ignoring setup.py in testing to conftest.py
    The config setting in setup.py was being ignored with the warning:
    `PytestConfigWarning: Unknown config option: collect_ignore`
    
    This moves it to conftest.py, which is the location recommended in the docs:
    https://docs.pytest.org/en/4.6.x/reference.html#collect-ignore

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,10 @@ from deepforest import utilities
 from deepforest import get_data
 import os
 
+collect_ignore = ['setup.py']
+
 @pytest.fixture(scope="session")
 def download_release():
     print("running fixtures")
     utilities.use_release()
     assert os.path.exists(get_data("NEON.pt"))
-    
