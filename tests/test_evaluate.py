fc1e20e11814dd055e96e463f66929b6b68f5a9e
bw4sz <benweinstein2010@gmail.com>
2023-07-11 16:33:59 -0700

    bug fix to close #441, only boxes which match should be included in class recall

--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -83,3 +83,23 @@ def test_evaluate_empty():
     
     df = pd.read_csv(csv_file)
     assert results["results"].shape[0] == df.shape[0]
+
+@pytest.fixture
+def sample_results():
+    # Create a sample DataFrame for testing
+    data = {
+        'true_label': [1, 1, 2],
+        'predicted_label': [1, 2, 1]
+    }
+    return pd.DataFrame(data)
+
+def test_compute_class_recall(sample_results):
+    # Test case with sample data
+    expected_recall = pd.DataFrame({
+        'label': [1, 2],
+        'recall': [0.5, 0],
+        'precision': [0.5, 0],
+        'size': [2, 1]
+    }).reset_index(drop=True)
+
+    assert evaluate.compute_class_recall(sample_results).equals(expected_recall)
