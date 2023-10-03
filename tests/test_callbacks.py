ee799ce07b9763d552ee085742cb5785788c136b
bw4sz <benweinstein2010@gmail.com>
2023-10-03 10:37:50 -0700

    checking out old commit

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -14,8 +14,9 @@ def m(download_release):
     m = main.deepforest()
     m.config["train"]["csv_file"] = get_data("example.csv") 
     m.config["train"]["root_dir"] = os.path.dirname(get_data("example.csv"))
-    m.config["train"]["fast_dev_run"] = True
+    m.config["train"]["fast_dev_run"] = False
     m.config["batch_size"] = 2
+    m.config["workers"] = 0
     m.config["validation"]["csv_file"] = get_data("example.csv") 
     m.config["validation"]["root_dir"] = os.path.dirname(get_data("example.csv"))
     
@@ -43,7 +44,7 @@ def test_log_images_multiclass(m, tmpdir):
 
     im_callback = callbacks.images_callback(csv_file=m.config["validation"]["csv_file"], root_dir=m.config["validation"]["root_dir"], savedir=tmpdir)
     m.create_trainer(callbacks=[im_callback])
-    m.max_steps = 2
+    m.max_steps = 1
     m.trainer.fit(m)
     saved_images = glob.glob("{}/*.png".format(tmpdir))
     assert len(saved_images) == 1
