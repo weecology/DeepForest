7aa4f61e15f548e32bddd2f08f1b9e895ca53725
JSpencerPittman <jspencerpittman@gmail.com>
2023-10-15 14:02:40 -0500

    Added clarity and security for new geometry_type parameter

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -70,7 +70,7 @@ def test_shapefile_to_annotations(tmpdir):
     
     gdf.to_file("{}/annotations.shp".format(tmpdir))
     image_path = get_data("OSBS_029.tif")
-    shp = utilities.shapefile_to_annotations(shapefile="{}/annotations.shp".format(tmpdir), rgb=image_path, savedir=tmpdir, geometry_type="boundingbox")
+    shp = utilities.shapefile_to_annotations(shapefile="{}/annotations.shp".format(tmpdir), rgb=image_path, savedir=tmpdir, geometry_type="bbox")
     assert shp.shape[0] == 2
     
 def test_boxes_to_shapefile_projected(m):
