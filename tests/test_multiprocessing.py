dc6d3e54018d22a9c73ff61ee2e2aba6fcd0b4c0
bw4sz <benweinstein2010@gmail.com>
2024-02-17 09:46:34 -0500

    issue #404, can we assert that workers are correctly connected to predict_dataloader and that torch.multiprocessing actually uses them?

---
+++ tests/test_multiprocessing.py
@@ -0,0 +1,21 @@
+# Ensure that multiprocessing is behaving as expected. 
+from deepforest import main, get_data
+from deepforest import dataset
+import pytest
+import os
+
+@pytest.mark.parametrize("num_workers",[0, 1 ,2])
+def test_predict_tile_workers(m, num_workers):
+    m.config["workers"] == num_workers
+    csv_file = get_data("OSBS_029.csv")
+    # make a dataset
+    ds = dataset.TreeDataset(csv_file=csv_file,
+                                 root_dir=os.path.dirname(csv_file),
+                                 transforms=None,
+                                 train=False)
+    dataloader = m.predict_dataloader(ds)
+    assert dataloader.num_workers == num_workers
+    
+
+    
+
