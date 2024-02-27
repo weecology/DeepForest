a2cfb2b36038e9cc7561e6fb5a83b6504b7224a2
bw4sz <benweinstein2010@gmail.com>
2024-02-27 15:01:36 -0800

    add tests directly from config

--- tests/test_multiprocessing.py
+++ tests/test_multiprocessing.py
@@ -1,10 +1,14 @@
-# Ensure that multiprocessing is behaving as expected. 
+# Ensure that multiprocessing is behaving as expected.
 from deepforest import main, get_data
 from deepforest import dataset
+from deepforest.utilities import read_config
+
 import pytest
 import os
+import shutil
+import yaml
 
-@pytest.mark.parametrize("num_workers",[0, 1 ,2])
+@pytest.mark.parametrize("num_workers",[0 ,2])
 def test_predict_tile_workers(m, num_workers):
     # Default workers is 0
     original_workers = m.config["workers"]
@@ -14,12 +18,31 @@ def test_predict_tile_workers(m, num_workers):
     csv_file = get_data("OSBS_029.csv")
     # make a dataset
     ds = dataset.TreeDataset(csv_file=csv_file,
+                             root_dir=os.path.dirname(csv_file),
+                             transforms=None,
+                             train=False)
+    dataloader = m.predict_dataloader(ds)
+    assert dataloader.num_workers == num_workers
+
+def test_predict_tile_workers_config(tmpdir):
+    # Open config file and change workers to 1, save to tmpdir
+    config_file = get_data("deepforest_config.yml")
+    tmp_config_file = os.path.join(tmpdir, "deepforest_config.yml")
+
+    shutil.copyfile(config_file, tmp_config_file)
+    x = read_config(tmp_config_file)
+    x["workers"] = 1
+    with open(tmp_config_file, "w+") as f:
+        f.write(yaml.dump(x))
+        
+    m = main.deepforest(config_file=tmp_config_file)
+    csv_file = get_data("OSBS_029.csv")
+    # make a dataset
+    ds = dataset.TreeDataset(csv_file=csv_file,
                                  root_dir=os.path.dirname(csv_file),
                                  transforms=None,
                                  train=False)
     dataloader = m.predict_dataloader(ds)
-    assert dataloader.num_workers == num_workers
-    
-
+    assert dataloader.num_workers == 1
     
 
