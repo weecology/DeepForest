7de55c1a941ab53dc06158cb84f8fd3ca5ca5b0a
Benjamin Weinstein <bw4sz@users.noreply.github.com>
2024-05-23 03:02:35 -0700

    log training loss in pytorch trainer (#673)
    Start logging loss again. This got lost with the upgrade to PyTorch 2.0

--- deepforest/main.py
+++ deepforest/main.py
@@ -533,6 +533,13 @@ class deepforest(pl.LightningModule):
         # sum of regression and classification loss
         losses = sum([loss for loss in loss_dict.values()])
 
+        # Log loss
+        for key, value in loss_dict.items():
+            self.log("train_{}".format(key), value, on_epoch=True)
+
+        # Log sum of losses
+        self.log("train_loss", losses, on_epoch=True)
+
         return losses
 
     def validation_step(self, batch, batch_idx):
