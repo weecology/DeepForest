2fb232c5a53dc13014068804ec54ca4533fb043f
Om Doiphode <omdoiphode161@gmail.com>
2024-08-19 10:15:36 +0530

    Fixed issue related to the bounding box coordinates

--- deepforest/preprocess.py
+++ deepforest/preprocess.py
@@ -91,6 +91,13 @@ def select_annotations(annotations, window):
 
     clipped_annotations.geometry = clipped_annotations.geometry.translate(
         xoff=-window_xmin, yoff=-window_ymin)
+    
+    # Update xmin, ymin, xmax, ymax based on the clipped annotations' geometry
+    if clipped_annotations.shape[0] > 0:
+      clipped_annotations['xmin'] = clipped_annotations.geometry.bounds.minx
+      clipped_annotations['ymin'] = clipped_annotations.geometry.bounds.miny
+      clipped_annotations['xmax'] = clipped_annotations.geometry.bounds.maxx
+      clipped_annotations['ymax'] = clipped_annotations.geometry.bounds.maxy
 
     return clipped_annotations
 
