01f5f4f298a4f9eabce5228423e2a1ae3e4833c0
bw4sz <benweinstein2010@gmail.com>
2024-12-17 17:01:51 -0800

    docs

--- tests/test_main.py
+++ tests/test_main.py
@@ -173,6 +173,7 @@ def test_validation_step(m):
     val_loss = m.validation_step(batch, 0)
     assert val_loss != 0
 
+"
 def test_validation_step_empty():
     """If the model returns an empty prediction, the metrics should not fail"""
     m = main.deepforest()
@@ -185,6 +186,8 @@ def test_validation_step_empty():
     m.predictions = []
     val_loss = m.validation_step(batch, 0)
     assert len(m.predictions) == 0
+
+    assert m.iou_metric.compute()["iou"] is None
 
 def test_validate(m):
     m.trainer = None
