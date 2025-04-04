8a9af761fd066e1ca97a8cca267e9069935be817
Keerthi Reddy <122682963+reddykkeerthi@users.noreply.github.com>
2025-04-04 21:46:10 +0530

    Checking for empty dataset (#1006)
    * Checking for empty dataset
    
    * fixed indentation

--- src/deepforest/main.py
+++ src/deepforest/main.py
@@ -300,6 +300,10 @@ class deepforest(pl.LightningModule, PyTorchModelHubMixin):
                                  transforms=self.transforms(augment=augment),
                                  label_dict=self.label_dict,
                                  preload_images=self.config["train"]["preload_images"])
+        if len(ds) == 0:
+            raise ValueError(
+                f"Dataset from {csv_file} is empty. Check CSV for valid entries and columns."
+            )
 
         data_loader = torch.utils.data.DataLoader(
             ds,
