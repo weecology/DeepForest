5a027ba49543f5205594fc1b3ab91f4b5fb3d903
bw4sz <benweinstein2010@gmail.com>
2025-05-20 10:21:35 -0700

    more explicit multi-processing tests

--- tests/test_multiprocessing.py
+++ tests/test_multiprocessing.py
@@ -19,12 +19,31 @@ def test_predict_tile_workers(m, num_workers):
     dataloader = m.predict_dataloader(ds)
     assert dataloader.num_workers == num_workers
 
+
 @pytest.mark.parametrize("num_workers", [0, 2])
-def test_predict_tile_workers_config(num_workers):
+@pytest.mark.parametrize("dataset_class", [
+    prediction.FromCSVFile,
+    prediction.SingleImage,
+    prediction.MultiImage,
+    prediction.TiledRaster,
+])
+def test_predict_tile_workers_config(num_workers, dataset_class):
     m = main.deepforest(config_args={"workers": num_workers})
     csv_file = get_data("OSBS_029.csv")
-    # make a dataset
-    ds = prediction.FromCSVFile(csv_file=csv_file,
-                             root_dir=os.path.dirname(csv_file))
+    root_dir = os.path.dirname(csv_file)
+    
+    # Create dataset based on class
+    if dataset_class == prediction.FromCSVFile:
+        ds = dataset_class(csv_file=csv_file, root_dir=root_dir)
+    elif dataset_class == prediction.SingleImage:
+        image_path = os.path.join(root_dir, "OSBS_029.png")
+        ds = dataset_class(path=image_path)
+    elif dataset_class == prediction.MultiImage:
+        image_path = os.path.join(root_dir, "OSBS_029.png")
+        ds = dataset_class(paths=[image_path], patch_size=400, patch_overlap=0.1)
+    else:  # TiledRaster
+        image_path = os.path.join(root_dir, "test_tiled.tif")
+        ds = dataset_class(path=image_path, patch_size=400, patch_overlap=0.1)
+        
     dataloader = m.predict_dataloader(ds)
     assert dataloader.num_workers == num_workers
