a89d4f4fddcfb092b4b4008e6529fac7c5f86528
ramiro <ramiguevara@gmail.com>
2026-07-29 13:37:25 -0300

    test: add lightweight offline assertions for train and predict batch sizes

--- tests/test_batch_size.py
+++ tests/test_batch_size.py
@@ -4,7 +4,13 @@ from deepforest import get_data, main
 
 
 def test_default_batch_sizes():
-    model = main.deepforest(config_args={"model": {"name": None}})
+    model = main.deepforest(
+        config_args={
+            "model": {"name": None},
+            "num_classes": 1,
+            "label_dict": {"Tree": 0},
+        }
+    )
     assert model.config.train_batch_size == 2
     assert model.config.predict_batch_size == 8
 
@@ -13,6 +19,8 @@ def test_custom_batch_sizes():
     custom_m = main.deepforest(
         config_args={
             "model": {"name": None},
+            "num_classes": 1,
+            "label_dict": {"Tree": 0},
             "train_batch_size": 4,
             "predict_batch_size": 16,
         }
@@ -22,7 +30,13 @@ def test_custom_batch_sizes():
 
 
 def test_train_dataloader_batch_size():
-    model = main.deepforest(config_args={"model": {"name": None}})
+    model = main.deepforest(
+        config_args={
+            "model": {"name": None},
+            "num_classes": 1,
+            "label_dict": {"Tree": 0},
+        }
+    )
     csv_file = get_data("OSBS_029.csv")
     root_dir = os.path.dirname(csv_file)
     model.config.train.csv_file = csv_file
@@ -32,7 +46,13 @@ def test_train_dataloader_batch_size():
 
 
 def test_val_dataloader_batch_size():
-    model = main.deepforest(config_args={"model": {"name": None}})
+    model = main.deepforest(
+        config_args={
+            "model": {"name": None},
+            "num_classes": 1,
+            "label_dict": {"Tree": 0},
+        }
+    )
     csv_file = get_data("OSBS_029.csv")
     root_dir = os.path.dirname(csv_file)
     model.config.validation.csv_file = csv_file
@@ -44,7 +64,13 @@ def test_val_dataloader_batch_size():
 def test_predict_dataloader_batch_size():
     from deepforest.datasets import prediction
 
-    model = main.deepforest(config_args={"model": {"name": None}})
+    model = main.deepforest(
+        config_args={
+            "model": {"name": None},
+            "num_classes": 1,
+            "label_dict": {"Tree": 0},
+        }
+    )
     csv_file = get_data("OSBS_029.csv")
     root_dir = os.path.dirname(csv_file)
     ds = prediction.FromCSVFile(csv_file=csv_file, root_dir=root_dir)
